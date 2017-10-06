0893a8621d507768a90df846cc1f8305f9e64c42
Phil McCown <pjlmac@gmail.com>
2017-10-06 11:11:19 -0400

    First part of Part 1 fixed

--- Exercise7-1.py
+++ Exercise7-1.py
@@ -1,13 +1,17 @@
 Ex7=open("Lecture11.fasta", "r")
 sequenceID=[]
 sequenceLength=[]
+percentGC=[]
 for line in Ex7:
     line=line.strip()
     if '>' in line:
         sequenceID.append(line[1:])
     else:
-        seqLen=len(line)
+        seqLen=float(len(line))
         G=line.count("G")
         C=line.count("C")
     sequenceLength.append(seqLen)
+    percentGC.append((G+C)/seqLen*100)
+print sequenceLength
+print percentGC
         
