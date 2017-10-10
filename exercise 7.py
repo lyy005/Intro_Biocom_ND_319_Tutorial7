d7ee4b5e44bf6e94b176c88662858e5003f9570c
omegadan01 <dbruzzes@nd.edi>
2017-10-10 12:17:41 -0400

    q3 barplot

--- exercise 7.py
+++ exercise 7.py
@@ -17,21 +17,22 @@
 
 
 
-#question 3
-#making the plot
+#########question 3################
 from plotnine import *
-
-import pandas
-dat = pandas.read_csv("data.txt")
+import pandas as pd
+dat = pd.read_csv("data.txt")
 
 #barplot  for mean observations in a region
-dat_grp= dat['observations'].groupby(dat['region']) #group observations by region
-dat_mean= dat_grp.mean() # mean of the dat grp into a list
+grouped= dat.groupby(["region"]).mean().reset_index() #mean observations by region
+print grouped
+grouped.columns = ['region', 'mean_observations']
+p= (ggplot(data=grouped)
+    + aes(x='region', y= 'mean_observations',fill= 'region')
+    + geom_bar(stat = "identity")
+    + theme_classic()
+    )
+print p
 
-df = pandas.DataFrame({'col':dat_mean})  #turn list into a dataframe
-print (df)
-print df[0:4]
-#only has one row....
 
 
 
