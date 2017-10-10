d28b56599388b47dff5707081f7ffe84de3b641d
Michelle Corley <mcorley1@nd.edu>
2017-10-09 22:13:29 -0400

    histogram of sequence length

--- part1script.py
+++ part1script.py
@@ -1,4 +1,7 @@
+import numpy
 import pandas
+from plotnine import *
+
 
 #Question 1
 InFile=open("Lecture11.fasta","r")
@@ -11,9 +14,10 @@ meltingTemp=[]
 
 #loop through each line in fasta file to process sequences
 for Line in InFile:
-    Line=Line.strip()
+    Line=Line.strip() #removes white space, tab, space, newline characters
     if '>' in Line:
         sequenceID.append(Line[1:])
+        #print(Line[1:])
     else:
         seqLen=float(len(Line))
         nG=Line.count("G")
@@ -22,10 +26,17 @@ for Line in InFile:
     #append values to lists
         sequenceLength.append(seqLen)
         percentGC.append((nG+nC)/seqLen*100)
-        print(percentGC)
     
 #combine lists into dataframe 
 seqDF = pandas.DataFrame(list(zip(sequenceID,sequenceLength,percentGC)),columns=['sequenceID','sequenceLength','percentGC'])
+#min(seqDF.sequenceLength)
 
 #close file
 InFile.close()
+
+#plots histogram of sequence length
+b=ggplot(seqDF,aes(x="sequenceLength"))
+b+geom_histogram(binwidth=5)+theme_classic()
+
+
+
