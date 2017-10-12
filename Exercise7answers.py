35a4c22bfd9e5e65cc578493e476642714a5ede0
Joe C <jchambe5@nd.edu>
2017-10-12 14:25:56 -0400

    question 1 complete. no question 2. working on question 3 part 1 with part 2 finished.JC

--- Exercise7answers.py
+++ Exercise7answers.py
@@ -1,3 +1,8 @@
+# Exercise 7
+
+
+#Question 1
+
 # open fasta file
 InFile=open("Lecture11.fasta","r")
 
@@ -41,4 +46,49 @@ histogram2+geom_histogram()+theme_classic()
 # changing colors and bins
 histogram2+geom_histogram(binwidth=15,fill='yellow',color='black')+theme_classic()
 
+#Question 2
+
+
+#Question 3
+
+# open data file
+q3north=0
+q3south=0
+q3west=0
+q3east=0
+
+
+for i in range(0,len(q3),1):
+    if q3.region[i]=="north":
+        q3north=q3north+q3.observations[i]
+    elif q3.region[i]=="south":
+        q3south=q3south+q3.observations[i]
+    elif q3.region[i]=="east":
+        q3east=q3east+q3.observations[i]
+    elif q3.region[i]=="west":
+        q3west=q3west+q3.observations[i]
+
+q3north
+q3south
+q3west
+q3east
+
+avgnorth=(sum(q3.region=="north")/len(q3north))
+sum(q3.region=="south")
+sum(q3.region=="east")
+sum(q3.region=="west")
+
+
+import pandas
+q3=pandas.read_csv("data.txt", sep=",", header=0)
+average=q3.groupby('region')['observations'].mean()
+
+regions=q3.groupby('region')['region']
+seqDF=pandas.DataFrame(list(zip(average)),columns=['regions','observations'] set='\t')
+df=average.to_frame(name=none)
+
+#scatter plot with jitter applied
+from plotnine import *
+q3sp=ggplot(q3,aes(x="region",y="observations"))
+q3sp+geom_jitter()+coord_cartesian()
 
