f15103785e2c1233250d73de504612218e9db728
Soren Holm <sholm@prudence.campus.nd.edu>
2017-10-11 21:16:14 -0400

    Problem 1 complete

--- exercise7.py
+++ exercise7.py
@@ -9,4 +9,13 @@ for line in InFile:
     else: 
         sequenceLength.append(len(line)-1)
         percentGC.append(1.0*(line.count("G")+line.count("C"))/len(line))
-print(percentGC)
+#print(percentGC)
+
+#So we need the data in a datafram to be used by ggplot aparantly. So here I put it in a dataframe
+data=pandas.DataFrame({"Sequence Length": sequenceLength, "Percent GC": percentGC})
+
+length=ggplot(data,aes(x="Sequence Length"))
+length+geom_histogram()+theme_classic()
+
+gc=ggplot(data,aes(x="Percent GC"))
+gc+geom_histogram()+theme_classic()
