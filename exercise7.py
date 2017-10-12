c3b3028561b02bb174148d9d189900a5b7ced6de
Mati Nemera <mnemera@nd.edu>
2017-10-12 00:25:52 -0400

    exercise7 question 1

--- exercise7.py
+++ exercise7.py
@@ -3,17 +3,17 @@ InFile=open("Lecture11.fasta","r")
 sequenceLength=[]
 percentGC = []
 for line in InFile:
-    line = line.strip()
+    line = line.strip() #remove extra space
     if ">" in line:
         next
     else: 
         sequenceLength.append(len(line)-1)
         percentGC.append(1.0*(line.count("G")+line.count("C"))/len(line))
-#print(percentGC)
+print(percentGC)
 
-#So we need the data in a datafram to be used by ggplot aparantly. So here I put it in a dataframe
+#Puts data in dataframe
 data=pandas.DataFrame({"Sequence Length": sequenceLength, "Percent GC": percentGC})
-
+from plotnine import *
 length=ggplot(data,aes(x="Sequence Length"))
 length+geom_histogram()+theme_classic()
 
