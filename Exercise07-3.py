722fd18d8ec40a17e45c016c47e2070842b4aa80
kkilgoreND <31992787+kkilgoreND@users.noreply.github.com>
2017-10-12 20:21:41 -0400

    Part 3 fourth commit

--- Exercise07-3.py
+++ Exercise07-3.py
@@ -10,14 +10,15 @@ data=pandas.read_csv("data.txt")
 means=ggplot('data')+theme_classic()+xlab("Populations")+ylab("Mean Number of Observations")
 +means+geom_bar(aes(x="factor(region)",y="observations",fill="region"),stat="summary",fun_y=numpy.mean)+ggtitle("Population Means")
 
-#Barplot means calculated for check
+#Barplot means calculated for check(not necessary)
 data.groupby(['region'])['observations'].mean()
 #means are slightly different
 
 #Scatterplot for the observations
-scatter=ggplot('data',aes('observations','region'))
+scatter=ggplot(data,aes('observations','region'))
 scatter+geom_jitter(aes(color='factor(region)'))+theme_classic()+ggtitle('All Observations')
-#Scatter plot shows that although the average observations seem to be similar across the regions, the observation distributions are different.
+#could have used scatter+geom_jitter()+coord_cartesian() instead
+#Scatterplot shows that although the average observations seem to be similar across the regions, the observation distributions are different.
 
 #Why?
 #Barplot shows the means for the regional observations whilst the scatterplot shows the values for the all of the observations from each region.
